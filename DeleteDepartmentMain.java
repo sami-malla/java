4a529407faaf94a5817da25c496c0e8e9bb75092
sami-malla <58032841+sami-malla@users.noreply.github.com>
2019-12-03 18:08:25 +0530

    Add files via upload

---
+++ DeleteDepartmentMain.java
@@ -0,0 +1,21 @@
+package com.hcl.department;
+
+import java.util.Scanner;
+
+
+
+public class DeleteDepartmentMain {
+	public static void main(String[] args) {
+		int deptno;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter employ no..");
+	    deptno=sc.nextInt();
+	    DepartmentDAO dao=new DepartmentDAO();
+		System.out.println(dao.deleteDepartment(deptno));
+	
+		
+		
+		
+	}
+
+}
